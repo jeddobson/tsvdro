5be4a2d33dd1eeaedfb1dd6cd678bd4265b7a74c
James Dobson <james.e.dobson@dartmouth.edu>
2018-08-09 17:45:19 -0400

    updating description and url for package

--- tsvdro/__init__.py
+++ tsvdro/__init__.py
@@ -1,1 +1,11 @@
+"""
+Data-Rich Objects for the Humanities
+==================================
+
+tsvdro is a Python module implementing an open standard for sharing
+data for text mining in the humanities.
+
+See https://github.com/jeddobson/tsvdro for package source and documentation
+"""
+
 name = "tsvdro"
